a4d22cf805f6e8e856663fb3edd612df4d5648f3
Shitanshu <shitanshu.19p@gmail.com>
2019-09-20 02:28:41 +0530

    implmented firebase utils, included ability to store authenticated user into our firebase database.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,34 @@ const config = {
     appId: process.env.REACT_APP_APP_ID
 };
 
+// Take user from Auth and store in DataBase
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    // Check user
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
