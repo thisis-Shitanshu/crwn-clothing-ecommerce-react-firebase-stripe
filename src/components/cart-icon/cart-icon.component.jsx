70d736c35cc5f6fb96841449669f372533b588d6
Shitanshu <shitanshu.19p@gmail.com>
2019-09-22 17:14:29 +0530

    added checkout page, checkout item component and updated cart to use selectors from reselect.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toogleCartHidden: () => dispatch(toogleCartHidden())
 });
 
-const mapStateToProps = state => ({
-    itemCount: selectCartItemCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemCount
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
