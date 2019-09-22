e1fa41474147a1a55ed0f0e0bf1a267a5fe924ff
Shitanshu <shitanshu.19p@gmail.com>
2019-09-22 22:52:07 +0530

    added shop reducer and directory reducer to seperate directory and shop data from their respective component and move them to redux store. converted shop page collection overview into its own componenet, making shop page convinient for routing implementation.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,36 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import SHOP_DATA from './shop.data';
-import './shop.styles.scss';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionOverview />
+    </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({
-                        id, 
-                        ...otherCollectionProps
-                    }) => (
-                        <CollectionPreview
-                            key={id}
-                            {...otherCollectionProps}
-                        />
-                    ))
-                }
-            </div>
-        );
-    }
-}
 
 export default ShopPage;
