da2d06b149286478b745be65cdc904346f4fb38d
Shitanshu <shitanshu.19p@gmail.com>
2019-10-06 16:08:04 +0530

    Implementing GraphQL Apollo in container for each component.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
-import App from './App';
+import { default as App } from './App/App.container';
+import { resolvers, typeDefs } from './graphql/resolvers';
+import { default as data } from './graphql/initial-data';
 
 const httpLink = createHttpLink({
   uri: 'https://crwn-clothing.com'
@@ -21,8 +23,12 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers
 });
+
+client.writeData({ data });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
